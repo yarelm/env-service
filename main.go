e84aceeadbc009b1a19f7bfef63c18f13819f605
Yarel Maman <yarel@doit-intl.com>
2021-01-28 15:15:35 +0200

    simplify

--- main.go
+++ main.go
@@ -5,7 +5,6 @@ import (
 	"database/sql"
 	"fmt"
 	"log"
-	"net/http"
 	"os"
 	"os/signal"
 	"syscall"
@@ -16,43 +15,27 @@ import (
 
 func main() {
 	log.Print("starting server...")
-	http.HandleFunc("/", handler)
-
-	// Determine port for HTTP service.
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "8080"
-		log.Printf("defaulting to port %s", port)
-	}
 
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
-
-	sigs := make(chan os.Signal, 1)
-	done := make(chan bool, 1)
-	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
-
-	go func() {
-		sig := <-sigs
-		log.Printf("got signal: %v", sig)
-		cancel()
-		close(done)
-	}()
-
-	go func() {
-		// Start HTTP server.
-		log.Printf("listening on port %s", port)
-		if err := http.ListenAndServe(":"+port, nil); err != nil {
-			log.Fatal(err)
-		}
-	}()
+	handleSignals(cancel)
 
 	db := connectDb(ctx)
 	defer db.Close()
 
-	consume(ctx)
-	<-done
-	fmt.Printf("bye!")
+	consumeMessages(ctx, db)
+	log.Printf("going down. bye!")
+}
+
+func handleSignals(doneFunc func()) {
+	signalC := make(chan os.Signal, 1)
+	signal.Notify(signalC, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		sig := <-signalC
+		log.Printf("got signal: %v", sig)
+		doneFunc()
+	}()
 }
 
 func connectDb(ctx context.Context) *sql.DB {
@@ -70,12 +53,11 @@ func connectDb(ctx context.Context) *sql.DB {
 		log.Fatal(err)
 	}
 
-	fmt.Println("Successfully connected to DB!")
-
+	log.Println("Successfully connected to DB!")
 	return db
 }
 
-func consume(ctx context.Context) {
+func consumeMessages(ctx context.Context, db *sql.DB) {
 	gcpProject := os.Getenv("GCP_PROJECT")
 	pubsubSubscription := os.Getenv("PUBSUB_SUBSCRIPTION")
 
@@ -87,20 +69,12 @@ func consume(ctx context.Context) {
 	}
 
 	sub := client.Subscription(pubsubSubscription)
-	perms, err := sub.IAM().TestPermissions(ctx, []string{
-		"pubsub.subscriptions.consume",
-	})
-	if err != nil {
-		log.Fatal(err)
-	}
-	for _, perm := range perms {
-		log.Printf("Allowed: %v\n", perm)
-	}
-
-	log.Print("started listening to pubsub...")
+	log.Printf("started listening to pubsub subscription %v...", pubsubSubscription)
 
 	err = sub.Receive(ctx, func(ctx context.Context, m *pubsub.Message) {
 		log.Printf("Got message: %s", m.Data)
+
+		// TODO: write to DB
 		m.Ack()
 	})
 	if err != nil {
@@ -108,12 +82,4 @@ func consume(ctx context.Context) {
 	}
 
 	log.Print("done listening to pubsub")
-}
-
-func handler(w http.ResponseWriter, r *http.Request) {
-	name := os.Getenv("NAME")
-	if name == "" {
-		name = "World"
-	}
-	fmt.Fprintf(w, "Hello %s!\n", name)
 }
